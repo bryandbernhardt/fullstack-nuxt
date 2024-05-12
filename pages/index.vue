455696397495518fd7a3daafbd36576ce897d08a
Bryan Dietrich Bernhardt <bryandbernhardt@gmail.com>
2024-05-12 20:43:41 -0300

    feat(index): fetch APIs

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script setup>
+// default fetch only works in client-side
+// const charizard = await fetch('https://pokeapi.co/api/v2/pokemon/charizard').then(res => res.json());
+
+// useAsyncData composable is responsible for wrapping async logic and returning the result once it is resolved.
+// const { data: charizard } = await useAsyncData('charizard', () => {
+//   // Vue $fetch works in server-side and client-side
+//   return $fetch('https://pokeapi.co/api/v2/pokemon/charizard')
+// })
+
+// Nuxt compact pattern for fetch API in client
+const charizard = await useFetch('https://pokeapi.co/api/v2/pokemon/charizard');
+</script>
+
+<template>
+  <h1>Home Page for Nuxt 3 Server Course</h1>
+  {{ charizard }}
+</template>
+
+<style scoped>
+
+</style>
