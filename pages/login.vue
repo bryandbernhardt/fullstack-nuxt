eacf04b063e82ecfd67a13765e41b2dd8061b3d6
Bryan Dietrich Bernhardt <bryandbernhardt@gmail.com>
2024-05-12 23:31:28 -0300

    feat(app): create server middleware

---
+++ pages/login.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const auth = useCookie('pokemon-auth');
+
+function authenticate() {
+  auth.value = '3gfs29ga6d';
+}
+
+function logout() {
+  auth.value = '';
+}
+</script>
+
+<template>
+  <h1>Login</h1>
+  <button @click="authenticate">Login</button>
+  <button @click="logout">Logout</button>
+</template>
+
+<style scoped>
+
+</style>
