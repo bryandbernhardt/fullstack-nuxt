0616aac0f17e575298cad2960fc81c718bb7adef
Bryan Dietrich Bernhardt <bryandbernhardt@gmail.com>
2024-05-12 23:59:30 -0300

    feat(nuxtConfig): add rendering strategies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  routeRules: {
+    '/': { prerender: true }, // SSG
+    '/login': { ssr: false }, // SPA
+    '/api/**': { cors: true }, // CORS to refuse outside requests
+  }
 })
